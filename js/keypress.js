2141ea882cc59a4474805627d237ada601496a96
Takanori Sono <takanori.sono@gmail.com>
2020-12-11 15:07:58 -0800

    Create Hero class with POF controls

--- js/keypress.js
+++ js/keypress.js
@@ -1,7 +1,13 @@
 console.log('loaded keypress.js');
 
+let keys = [];
+
 $(document).ready(function(){
   $(document).keydown((e)=> {
     console.log(e.which)
-  })
+    keys.push(e.which);
+  });
 });
+
+
+
--- js/sprite.js
+++ js/sprite.js
@@ -33,8 +33,45 @@ class Circle {
   }
 }
 
+class Hero extends Circle {
+  constructor(ctx, x, y, dx, dy, radius){
+    super(ctx, x, y, dx, dy, radius);
+  }
+
+  update(){
+    //use key press for hero
+    if (keys.length > 0) {
+      this.dx = this.radius;
+      this.dy = 0;
+      keys.shift();
+    }
+    if (this.x + this.radius >= this.ctx.canvas.clientWidth || this.x - this.radius < 0) {
+      this.dx = 0;
+    }
+
+    if (this.y + this.radius > this.ctx.canvas.clientHeight || this.y - this.radius < 0) {
+      this.dy = 0;
+    }
+
+    this.x += this.dx;
+    this.y += this.dy;
+
+    this.draw();
+
+  }
+}
+
 function animate(){
   requestAnimationFrame(animate);
   ctx1.clearRect(0, 0, ctx1.canvas.clientWidth, ctx1.canvas.clientHeight);
-  circle1.update();
+  
+  //update multiple
+  if (sprites.length > 1) {
+    sprites.forEach((sprite) => {
+      sprite.update();
+    })
+  } else {
+    circle1.update();
+  }
+  
 }
