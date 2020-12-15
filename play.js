7108cd79a7984027d831e234aedadf80778c1592
Takanori Sono <takanori.sono@gmail.com>
2020-12-15 14:03:11 -0800

    Remove and clean up code

--- play.js
+++ play.js
@@ -1,13 +1,6 @@
-// import {Circle} from "./js/sprite.js";
-
 let canvas = document.getElementById("game-canvas"),
     ctx1 = canvas.getContext("2d");
 
-let dx = Math.random() * 3;
-let dy = Math.random() * 3;
-
-// let circle1 = new Circle(ctx1, 300, 100, dx, dy, 30);
-// let circle2 = new Circle(ctx1, 100, 100, 0, 0, 20);
 let gameover = false;
 
 //idleChar
